89d0d2a5681040dca6ed190bc2e5584e31d41746
aGrIklololol <60868947+aGrIklololol@users.noreply.github.com>
2020-06-17 12:54:30 +0300

    Add files via upload
    Окончательная отвязка от библиотек, исправлены ошибки.

--- Listener.pas
+++ Listener.pas
@@ -1,9 +1,9 @@
-﻿uses EasyTCP, EasyUDP;
-
-var
-  NowTesting, IP, Message, Suffix: string;
+﻿var
+  NowTesting: string;
   Port: integer;
   DT: DateTime;
+  TL: System.Net.Sockets.TcpListener;
+  Buffer: array of byte;
 
 begin
   Write('Порт: ');
@@ -12,25 +12,40 @@ begin
   Readln(NowTesting);
   if (NowTesting = 'TCP') then
   begin
-    EasyTCP.InitializeListener(Port);
-    EasyTCP.StartListener;
-    while (not EasyTCP.Pending) do 
+    TL := System.Net.Sockets.TcpListener.Create(Port);
+    TL.Start;
+    while (not TL.Pending) do 
     begin
       writeln('Слушаю порт ', Port, '...');
       sleep(1000);
     end;
-    EasyTCP.AcceptConnectByListener;
+    var Client := TL.AcceptTcpClient;
+    var NS := Client.GetStream;
     Writeln('Есть контакт! Включаю чат... ');
     while True do
     begin
-      if EasyTCP.Available then
-        Writeln('<', DT.Hour, ':', DT.Minute, ':', DT.Second, '> ', EasyTCP.ByteToStr(EasyTCP.ReadFrom));
+      if NS.CanRead then
+      begin
+        var Message := new StringBuilder();
+        var BytesRead := 0;
+        while NS.DataAvailable do
+        begin
+          BytesRead := NS.Read(Buffer, 0, Buffer.Length);
+          Message.AppendFormat(Encoding.ASCII.GetString(Buffer, 0, BytesRead), Encoding.ASCII.GetString(Buffer, 0, BytesRead));
+        end;
+        DT := DateTime.Now;
+        Writeln('<', DT.Hour, ':', DT.Minute, ':', DT.Second, '> ', Message);
+      end;
     end;
   end;
   if (NowTesting = 'UDP') then
-  BEGIN
-    EasyUDP.Connect(System.Net.IPAddress.Any, Port);
-    Writeln('Ожидаю входящих сообщений на порт ', Port);
-    EasyUDP.Receive;
-  End;
+  begin
+    var Client := new System.Net.Sockets.UdpClient(Port);
+    var EP := new System.Net.IPEndPoint(System.Net.IPAddress.Any, 0);
+    while true do
+    begin
+      DT := DateTime.Now;
+      Writeln('<', DT.Hour, ':', DT.Minute, ':', DT.Second, '> ',  Encoding.ASCII.GetString(Client.Receive(EP)));
+    end;
+  end;
 end.
